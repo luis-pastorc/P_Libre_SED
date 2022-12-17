064892a849979d2fd8ee9094e0edad33c444aaf4
Luis Pastor Camarillo <luis.pastorc@edu.uah.es>
2022-12-17 18:58:51 +0100

    Reloj estable

--- PracticaLibre/Ficheros/Practica_libre.c
+++ PracticaLibre/Ficheros/Practica_libre.c
@@ -2,19 +2,15 @@
 
 uint32_t SystemFrequency=100000000;
 
-uint32_t cuenta_1ms,visualizar;  // Contador de ms 
-
-int NUMEROS[10] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90}; //0....9
-int HOLA[4] = {0x89,0xC0,0xC7,0x88};	//H O L A
-int tiempo[8] = {0,0,0,0,0,0,0,0}; //, centesimas de s,decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de hora, decenas de horas
-int alarma1[7] = {0,0,0,0,0,0,0}; //decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de hora, decenas de horas
-int alarma2[7] = {0,0,0,0,0,0,0}; //decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de hora, decenas de horas
-int temp1[7] = {0,0,0,0,0,0,0}; //decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de hora, decenas de horas
-int temp2[7] = {0,0,0,0,0,0,0}; //decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de hora, decenas de horas
+int NUMEROS[10]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90}; //0....9
+int HOLA[4]= {0x89,0xC0,0xC7,0x88};	//H O L A
+int tiempo[8]={0,0,0,0,0,0,0,0}; //centesimas de s, decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de h, decenas de h
+int alarma1[8]={0,0,0,0,0,0,0,0}; //centesimas de s, decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de h, decenas de h
+int alarma2[8]={0,0,0,0,0,0,0,0}; //centesimas de s, decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de h, decenas de h
+int temp1[8]={0,0,0,0,0,0,0,0}; //centesimas de s, decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de h, decenas de h
+int temp2[8]={0,0,0,0,0,0,0,0}; //centesimas de s, decimas de s, unidades de s, decenas de s, unidades de m, decenas de m, unidades de h, decenas de h
 int m;	//cambia cada 5ms para visualizar
-int interrupciones;
 int entradas;	//para gestionar el switch
-int switch_horas_seg;	//elegir horas+mins o segs+decs
 int switch_alarma_1;
 int switch_alarma_2;
 int switch_temp_1;
@@ -28,12 +24,15 @@ void config_GPIO (void)
 	LPC_PINCON->PINSEL4 |= 1 << (11*2); //EINT1 
 	LPC_PINCON->PINSEL4 |= 1 << (12*2);	//EINT2
 	
-  LPC_GPIO1->FIODIR |= (127<<20);				// P1.20 hasta P1.26 definido como salida
+  LPC_GPIO1->FIODIR |= (255<<19);				// P1.19 hasta P1.26 definido como salida
 	LPC_GPIO2->FIODIR |= (15<<0);					// P2.0 hasta P2.3 como salida
   LPC_GPIO2->FIODIR &= ~(1<<13);				// P2.13 definido como entrada - PULSADOR Reseteo timer
-	LPC_GPIO0->FIODIR &= ~(1<<3);					// P0.3 definido como entrada - SELECTOR
+	LPC_GPIO0->FIODIR &= ~(1<<2);					// P0.2 definido como entrada - Switch horas/segundos
+	LPC_GPIO0->FIODIR &= ~(1<<3);					// P0.3 definido como entrada - Switch texto/timer
+	
 	LPC_PINCON->PINMODE2 &= ~(3<<2*13);		//P2.13 a pull-up
 	LPC_PINCON->PINMODE0 &= ~(3<<2*3);		//P0.3 a pull-up
+	LPC_PINCON->PINMODE0 |= (3<<2*2);		//P0.2 a pull-down
 } 
 
 void config_Systick(void)
@@ -52,13 +51,11 @@ void config_Timer0(void)	//Interrumpe cada 5ms
 	LPC_TIM0 ->TCR |=(1<<0);
  }
  
-void configura_Interrupciones()
+void config_Interrupciones()
 {
-	//EINT0 Y EINT1 activas por flanco
-	//LPC_SC -> EXTMODE |= (1<<0);
-	//LPC_SC -> EXTMODE |= (1<<1);
-	//EINT0 ACTIVA POR SUBIDA Y EINT1 POR BAJADA
-	//LPC_SC -> EXTPOLAR |= (1<<0);		//EINT0 activa por flanco de subida
+	LPC_SC -> EXTMODE |= (1<<0);		//EINT0 activa por flanco
+	//LPC_SC -> EXTMODE |= (1<<1);	//EINT1 activa por flanco
+	LPC_SC -> EXTPOLAR |= (1<<0);		//EINT0 activa por flanco de subida
 	//LPC_SC -> EXTPOLAR &= ~(1<<1);	//EINT1 activa por flanco de bajada
 	LPC_SC -> EXTINT |= (0x07);			//flags
 	
@@ -96,6 +93,7 @@ void configura_Interrupciones()
 void SysTick_Handler(void)		//Gestiona los valores del reloj
 {
 	//tiempo[0] son las centesimas de s, que siempre valdrán 0
+	
 	tiempo[1]++;	//decima de segundo
 	if(tiempo[1]>=10)
 	{
@@ -137,69 +135,78 @@ void SysTick_Handler(void)		//Gestiona los valores del reloj
 	}
 }
 
-void EINT0_IRQHandler()
+void EINT0_IRQHandler()	//Controla la visualización
 {
-	interrupciones++;
+	LPC_SC->EXTINT |= (1);   // Borrar el flag de la EINT0 --> EXTINT.0
+	entradas++;
+	if (entradas>4)
+		entradas=0;
 }
 
 void EINT1_IRQHandler()
 {
-	interrupciones++;
+
 }
 
 void EINT2_IRQHandler()
 {
-	interrupciones++;
+
 }
 
 int main (void)
 { 
-	cuenta_1ms=0;
-  configura_Interrupciones();
 	config_GPIO();
+  config_Interrupciones();
 	config_Timer0();
 	config_Systick();
 	
 	while(1) 
 	{
-		if ((LPC_GPIO0->FIOPIN & (1<<3))==0)	//P0.3 == 0
+		if((LPC_GPIO0->FIOPIN & (1<<3))==0)	//P0.3==0 switch_texto_aviso
 		{
-			if(switch_horas_seg == 0)
-						x=0;	//muestra horas y minutos
-					else
-						x=0;	//muestrasegundos y decimas
+			
+			if((LPC_GPIO0->FIOPIN & (1<<2))==0)	//P0.2 switch_horas_seg
+					x=0;	//muestra segundos
+			else
+					x=4;	//muestra minutos y horas
 					
-			entradas = LPC_GPIO0->FIOPIN & 4;
+			/*if(m==2)
+				LPC_GPIO1->FIOPIN |= (1<<19); //Enciendo punto
+			else
+				LPC_GPIO1->FIOPIN &= ~(1<<19); //Apago punto
+			*/
+			
 			switch(entradas)
 			{
 				case 0:				//visualizar Timer
 					LPC_GPIO1->FIOPIN = (NUMEROS[tiempo[m+x]])<<20;
-					LPC_GPIO2->FIOPIN = (1 << (m)) & 0xF;
+					LPC_GPIO2->FIOPIN = (1 << (3-m)) & 0xF;
 					continue;
 				
 				case 1:				//visualizar Alarma_1
-					LPC_GPIO1->FIOPIN = (NUMEROS[alarma1[m+1]])<<20;
-					LPC_GPIO2->FIOPIN = (1 << (m)) & 0xF;
+					LPC_GPIO1->FIOPIN = (NUMEROS[alarma1[m+x]])<<20;
+					LPC_GPIO2->FIOPIN = (1 << (4-m)) & 0xF;
 					continue;
 				
 				case 2:				//visualizar Alarma_2
-					LPC_GPIO1->FIOPIN = (NUMEROS[alarma2[m+1]])<<20;
+					LPC_GPIO1->FIOPIN = (NUMEROS[alarma2[m+x]])<<20;
 					LPC_GPIO2->FIOPIN = (1 << (m)) & 0xF;
 					continue;
 				
 				case 3:				//visualizar Temporizador_1
-					LPC_GPIO1->FIOPIN = (NUMEROS[temp1[m+1]])<<20;
+					LPC_GPIO1->FIOPIN = (NUMEROS[temp1[m+x]])<<20;
 					LPC_GPIO2->FIOPIN = (1 << (m)) & 0xF;
 					continue;
 				
 				case 4:				//visualizar Temporizador_2
-					LPC_GPIO1->FIOPIN = (NUMEROS[temp2[m+1]])<<20;
+					LPC_GPIO1->FIOPIN = (NUMEROS[temp2[m+x]])<<20;
 					LPC_GPIO2->FIOPIN = (1 << (m)) & 0xF;
 					continue;
 			 }
 		}
 		else			//P0.3 == 1 Visualiza HOLA
 		{	
+				
 				LPC_GPIO1->FIOPIN = (HOLA[m])<<20;
 				LPC_GPIO2->FIOPIN = (1 << m) & 0xF;
 				continue;
